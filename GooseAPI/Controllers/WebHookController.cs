958135c5ff1c86986d8400aac3d17a10667db831
yotam blu <yotamblu@gmail.com>
2026-01-27 17:38:26 +0200

    moving webooks to api instead of old web client

---
+++ GooseAPI/Controllers/WebHookController.cs
@@ -0,0 +1,101 @@
+﻿using FireSharp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace GooseAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/webhook")]
+    public class WebHookController : Controller
+    {
+        [AllowAnonymous]
+        [HttpPost("workoutData")]
+        public async Task<IActionResult> SubmitWorkoutData()
+        {
+            using var reader = new StreamReader(Request.Body);
+            var rawJsonString = await reader.ReadToEndAsync();
+            LogActivityData(rawJsonString);
+            List<Workout> data = new ActivityJsonParser(rawJsonString).ParseActivityData();
+            await Console.Out.WriteLineAsync(data.Count == 0 ? "0" :"!0");
+            foreach (Workout workout in data)
+            {
+                LogActivityData(workout);
+            }
+
+            return Ok(new { message = "Workout Stored Successfully" });
+        }
+
+
+        [HttpPost("sleepData")] 
+        public IActionResult SubmitSleepData()
+        {
+            Request.EnableBuffering();
+
+            string rawJsonString = "";
+            // Use a StreamReader to read the body stream
+            using (var reader = new StreamReader(Request.Body, System.Text.Encoding.UTF8))
+            {
+                rawJsonString = reader.ReadToEndAsync().GetAwaiter().GetResult();
+
+                Request.Body.Position = 0;
+            }
+
+
+
+
+            Sleep sleep = JsonConvert.DeserializeObject<Root>(rawJsonString).Sleeps[0];
+            FirebaseClient client = new FirebaseClient(FireBaseConfig.config);
+
+            SleepData data = new SleepData
+            {
+                AwakeDurationInSeconds = sleep.AwakeDurationInSeconds,
+                DeepSleepDurationInSeconds = sleep.DeepSleepDurationInSeconds,
+                LightSleepDurationInSeconds = sleep.LightSleepDurationInSeconds,
+                OverallSleepScore = sleep.OverallSleepScore,
+                RemSleepInSeconds = sleep.RemSleepInSeconds,
+                SleepDate = sleep.CalendarDate,
+                SleepDurationInSeconds = sleep.DurationInSeconds,
+                SleepScores = sleep.SleepScores,
+                SleepStartTimeInSeconds = sleep.StartTimeInSeconds,
+                SleepTimeOffsetInSeconds = sleep.StartTimeOffsetInSeconds,
+                SummaryID = sleep.SummaryId
+
+
+            };
+
+            new FirebaseService().InsertData($"SleepData/{sleep.UserAccessToken}/{sleep.CalendarDate}", data);
+
+
+
+
+
+            return Ok();
+        }
+
+
+        
+
+
+        private void LogActivityData(Workout data)
+        {
+            FirebaseService service = new FirebaseService();
+            string path = $"Activities/{data.UserAccessToken}/{data.WorkoutId}";
+            Console.WriteLine(path);
+            service.InsertData(path, data);
+
+        }
+
+        private void LogActivityData(string data)
+        {
+            FirebaseClient client = new FirebaseClient(FireBaseConfig.pushConfig);
+            string path = $"{DateTime.Now.ToString().Replace("/","-")}";
+            Console.WriteLine(path);
+            client.Set(path, data);
+
+        }
+
+
+    }
+}
---
+++ GooseAPI/SleepClasses.cs
@@ -0,0 +1,61 @@
+﻿namespace GooseAPI
+{
+   
+   
+    public class Sleep
+    {
+        public string UserId { get; set; }
+        public string UserAccessToken { get; set; }
+        public string SummaryId { get; set; }
+        public string CalendarDate { get; set; }
+        public int DurationInSeconds { get; set; }
+        public int TotalNapDurationInSeconds { get; set; }
+        public long StartTimeInSeconds { get; set; }
+        public int StartTimeOffsetInSeconds { get; set; }
+        public int unmeasurableSleepInSeconds { get; set; }
+        public int DeepSleepDurationInSeconds { get; set; }
+        public int LightSleepDurationInSeconds { get; set; }
+        public int RemSleepInSeconds { get; set; }
+        public int AwakeDurationInSeconds { get; set; }
+        public object SleepLevelsMap { get; set; }
+        public string Validation { get; set; }
+        public Dictionary<string, object> TimeOffsetSleepSpo2 { get; set; }
+        public Dictionary<string, double> TimeOffsetSleepRespiration { get; set; }
+        public SleepScore OverallSleepScore { get; set; }
+        public Dictionary<string, Dictionary<string, string>> SleepScores { get; set; }
+    }
+
+   
+    public class SleepScoreKVP
+    {
+
+        public string Key { get; set; }
+        public SleepScore Value { get; set; }
+    }
+
+
+    public class SleepData
+    {
+        public string SummaryID { get; set; }
+        public int SleepDurationInSeconds { get; set; }
+        public long SleepStartTimeInSeconds { get; set; }
+        public int SleepTimeOffsetInSeconds { get; set; }
+        public string SleepDate { get; set; }
+        public int DeepSleepDurationInSeconds { get; set; }
+        public int LightSleepDurationInSeconds { get; set; }
+        public int RemSleepInSeconds { get; set; }
+        public int AwakeDurationInSeconds { get; set; }
+        public Dictionary<string, Dictionary<string, string>> SleepScores { get; set; }
+        public SleepScore OverallSleepScore { get; set; }
+        public string SleepScoreQualifier;
+
+    }
+
+    public class Root
+    {
+        public List<Sleep> Sleeps { get; set; }
+    }
+
+
+   
+}
